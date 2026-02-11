dde002842dce532c5ee85854596f72e141365551
k3shi08 <k3shi08@gamial.c0m>
2026-02-11 09:19:17 +0300

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,9 @@
+university = "JKUAT"
+grade = "A-"
+favourite_course = "Electrical engineering" 
+cluster_points = "42.7"
+
+print(f"my favourite university is {university}")
+print(f"i got {grade}")
+print(f"i want to pursue {favourite_course}")
+print(f"the required is {cluster_points}")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+# This is a comment
+print("Hello from Keshi ") 
+# city / town
+print("I am from Nyeri ") 
+# my hobbies
+print("I like to play volleyball,reading novels,watcing series") 
+
+age = 18
+height = 1.25
+weight = 75
+
+print(f" I am {age} years old")
+print(f" I am also {height} metres tall")
+print(f" And weigh {weight} kgs")
